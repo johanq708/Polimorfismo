508a119c624e1d3cb4046f35673b5ba6c5d20ecf
Estudiante <Estudiante@DESKTOP-KS3QO24>
2017-04-05 15:32:17 -0500

    polimorfismo

---
+++ Polimorfismo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polimorfismo;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Polimorfismo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        A d=new D();
+    }
+    
+}

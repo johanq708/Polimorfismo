e49a61d0941022dc020048c2cfa89f49e90dc612
Estudiante <Estudiante@DESKTOP-KS3QO24>
2017-04-05 15:32:32 -0500

    poli

---
+++ C.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polimorfismo;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class C extends A{
+    
+    public C(){
+        System.out.println("costructor de C");
+    }
+    public void fc(){
+        System.out.println("En C");
+    }
+}
